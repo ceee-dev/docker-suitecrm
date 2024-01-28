e0df2e88e658807205add2bc6049f07a9b8ab672
C-EEE ORG <146799720+ceeedevops@users.noreply.github.com>
2024-01-28 07:36:45 +0530

    Update create-env.sh

--- create-env.sh
+++ create-env.sh
@@ -1,5 +1,5 @@
 #/usr/bin/bash
 
-echo "COMPOSE_PROJECT_NAME=suitcrm" > .env
+echo "COMPOSE_PROJECT_NAME=suitecrm" > .env
 echo "MARIADB_PASSWORD=$(cat /dev/urandom | strings | grep -Eoa "[a-zA-Z0-9@$%&_]*" | head -n 10 | tr -d '\n'| cut -c1-16)" >> .env
 echo "TZ=Asia/Colombo" >> .env
