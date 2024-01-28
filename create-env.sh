e78372504bc2be4e1119d1b5f24132ac9b9c573e
C-EEE ORG <146799720+ceeedevops@users.noreply.github.com>
2024-01-28 06:50:11 +0530

    Update create-env.sh

--- create-env.sh
+++ create-env.sh
@@ -1,7 +1,6 @@
 #/usr/bin/bash
 
-echo "ROOT_ACCESS_PASSWORD=$(cat /dev/urandom | strings | grep -Eoa "[a-zA-Z0-9@$%&_]*" | head -n 10 | tr -d '\n'| cut -c1-16)" > .env
-echo "DATABASE_PASSWORD=$(cat /dev/urandom | strings | grep -Eoa "[a-zA-Z0-9@$%&_]*" | head -n 10 | tr -d '\n'| cut -c1-16)" >> .env
-echo "USER_DB_NAME=zerodb" >> .env  
-echo "MYSQL_USER=zerodba" >> .env
+
+echo "COMPOSE_PROJECT_NAME=suitcrm" > .env
+echo "MARIADB_PASSWORD=$(cat /dev/urandom | strings | grep -Eoa "[a-zA-Z0-9@$%&_]*" | head -n 10 | tr -d '\n'| cut -c1-16)" >> .env
 echo "TZ=Asia/Colombo" >> .env
